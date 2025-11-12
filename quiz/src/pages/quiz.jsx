68fba769ddd73cdb4c95b0f4c0e697c6e18e5016
BS4002 <bakkousalma@gmail.com>
2025-11-12 16:25:10 +0100

    affichage des réponses avec chaque question

--- quiz/src/pages/quiz.jsx
+++ quiz/src/pages/quiz.jsx
@@ -3,23 +3,23 @@ import questions from '../data/questions';
 import { useState } from 'react';
 
 export default function Quiz() {
-    const [indexQuestion,setIndexQuestion]=useState(0)
-
+    const [indexQuestion,setIndexQuestion]=useState(0);
+  
 
     const nextQuestion=()=>{
         setIndexQuestion(indexQuestion+1)
     }
 
 
-
   return (
-    <div>
-      <h2>Quiz</h2>
-      <h4>{questions[indexQuestion].question}</h4>
-      <button onClick={nextQuestion}>{questions[indexQuestion].options[0]}</button>
-      <button onClick={nextQuestion}>{questions[indexQuestion].options[1]}</button>
-      <button onClick={nextQuestion}>{questions[indexQuestion].options[2]}</button>
-      <button onClick={nextQuestion}>{questions[indexQuestion].options[3]}</button>     
+      <div>
+        <h1>{questions[indexQuestion].question}</h1>
+    {
+        questions[indexQuestion].options.map((option)=>(
+        <button onClick={()=>nextQuestion()}>{option}</button>
+    ))
+   }
+
     </div>
   )
 }
