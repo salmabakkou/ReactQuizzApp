213dbc75d1f75e78580895680d7089885e26fa81
BS4002 <bakkousalma@gmail.com>
2025-11-12 15:32:10 +0100

    affichage de question suivante

--- quiz/src/pages/quiz.jsx
+++ quiz/src/pages/quiz.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import questions from '../data/questions';
 import { useState } from 'react';
-import { use } from 'react';
 
 export default function Quiz() {
-    const [index,setIndex]=useState(0)
-    const [question,setQuestion]=useState(questions);
-    const [option,setOption]=useState([])
+    const [indexQuestion,setIndexQuestion]=useState(0)
+
+
+    const nextQuestion=()=>{
+        setIndexQuestion(indexQuestion+1)
+    }
+
 
 
   return (
     <div>
       <h2>Quiz</h2>
-      <h4>{questions[0].question}</h4>
-      <button>{questions[0].options[0]}</button>
-      <button>{questions[0].options[1]}</button>
-      <button>{questions[0].options[2]}</button>
-      <button>{questions[0].options[3]}</button>
+      <h4>{questions[indexQuestion].question}</h4>
+      <button onClick={nextQuestion}>{questions[indexQuestion].options[0]}</button>
+      <button onClick={nextQuestion}>{questions[indexQuestion].options[1]}</button>
+      <button onClick={nextQuestion}>{questions[indexQuestion].options[2]}</button>
+      <button onClick={nextQuestion}>{questions[indexQuestion].options[3]}</button>     
     </div>
   )
 }
