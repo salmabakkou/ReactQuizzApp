03f6c174ffd71e646cbcadf8283268ac7fb33fc6
BS4002 <bakkousalma@gmail.com>
2025-11-13 13:03:14 +0100

    navigation de home vers quiz

--- quiz/src/pages/home.jsx
+++ quiz/src/pages/home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 
 
 export default function Home() {
+  const navigate=useNavigate();
   return (
     <div>
         <h1>Home</h1>
+        <button onClick={() => navigate('/quiz')} >Start Quiz</button>
        
     </div>
   )
